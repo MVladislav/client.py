3b4f27395b56051b783264b38492c90f5338a40d
mvladislav <dev@mvladislav.online>
2023-11-18 15:15:22 +0100

    test: update auto empty

--- tests/commands/json/test_auto_empty.py
+++ tests/commands/json/test_auto_empty.py
@@ -64,6 +64,8 @@ async def test_GetAutoEmpty(json: dict[str, Any], expected: AutoEmptyModeEvent) 
             {"enable": 1, "frequency": "smart"},
             AutoEmptyModeEvent(True, AutoEmptyMode.MODE_SMART),
         ),
+        # NOTE: this test is also working, as 'enable' will set auto to 'true' if not provided
+        # as 'enable' is required when set a 'frequency'
         (
             (None, AutoEmptyMode.MODE_25),
             {"enable": 1, "frequency": "25"},
@@ -99,3 +101,35 @@ async def test_SetAutoEmpty(
         command = SetAutoEmpty(value[0], value[1])
 
     await assert_set_command(command, args, expected)
+
+
+@pytest.mark.parametrize(
+    ("value", "args", "expected"),
+    [
+        (
+            (None, AutoEmptyMode.MODE_AUTO),
+            {"enable": 0, "frequency": "auto"},
+            AutoEmptyModeEvent(False, AutoEmptyMode.MODE_AUTO),
+        ),
+        (
+            (None, None),
+            {"enable": 0, "frequency": "auto"},
+            AutoEmptyModeEvent(False, AutoEmptyMode.MODE_AUTO),
+        ),
+    ],
+)
+async def test_SetAutoEmptyFail(
+    value: tuple[bool | None, AutoEmptyMode | str | None],
+    args: dict[str, Any],
+    expected: AutoEmptyModeEvent,
+) -> None:
+    command = SetAutoEmpty()
+    if value[0] is None and value[1] is not None:
+        command = SetAutoEmpty(frequency=value[1])
+    elif value[1] is None and value[0] is not None:
+        command = SetAutoEmpty(enable=value[0])
+    elif value[0] is not None and value[1] is not None:
+        command = SetAutoEmpty(value[0], value[1])
+
+    with pytest.raises(AssertionError):
+        await assert_set_command(command, args, expected)
