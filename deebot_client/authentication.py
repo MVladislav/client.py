ec9dd7f3b87f20e118b5745b568d07a8fc4842e8
MVladislav <dev@mvladislav.online>
2024-02-08 22:50:42 +0100

    fix(#0): merge errors

--- deebot_client/authentication.py
+++ deebot_client/authentication.py
@@ -10,7 +10,7 @@ from urllib.parse import urljoin
 
 from aiohttp import ClientResponseError, ClientSession, hdrs
 
-from .const import COUNTRY_CHINA, PATH_USERS_USER, REALM
+from .const import COUNTRY_CHINA, REALM
 from .exceptions import (
     ApiError,
     ApiTimeoutError,
@@ -33,7 +33,6 @@ _CLIENT_KEY = "1520391301804"
 _CLIENT_SECRET = "6c319b2a5cd3e66e39159c2e28f2fce9"  # noqa: S105
 _AUTH_CLIENT_KEY = "1520391491841"
 _AUTH_CLIENT_SECRET = "77ef58ce3afbe337da74aa8c5ab963a9"  # noqa: S105
-
 _USER_LOGIN_PATH_FORMAT = "/v1/private/{country}/{lang}/{deviceId}/{appCode}/{appVersion}/{channel}/{deviceType}/user/login"
 _GLOBAL_AUTHCODE_PATH = "/v1/global/auth/getAuthCode"
 _PATH_USERS_USER = "users/user.do"
@@ -243,7 +242,7 @@ class _AuthClient:
         }
 
         for i in range(3):
-            resp = await self.post(PATH_USERS_USER, data)
+            resp = await self.post(_PATH_USERS_USER, data)
             if resp["result"] == "ok":
                 return resp
             if resp["result"] == "fail" and resp["error"] == "set token error.":
@@ -251,9 +250,9 @@ class _AuthClient:
                 _LOGGER.warning("loginByItToken set token error, attempt %d/3", i + 2)
                 continue
 
-            _LOGGER.error("call to %s failed with %s", PATH_USERS_USER, resp)
+            _LOGGER.error("call to %s failed with %s", _PATH_USERS_USER, resp)
             msg = (
-                f"failure {resp['error']} ({resp['errno']}) for call {PATH_USERS_USER}"
+                f"failure {resp['error']} ({resp['errno']}) for call {_PATH_USERS_USER}"
             )
             raise AuthenticationError(msg)
 
